d0b8129a01ee15672890a00e685b98c0c99a2f5e
strawloki <laurplayscevrea@gmail.com>
2022-12-06 09:13:13 +0100

    Added prompt for inputting how many values the program will be working with.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,28 @@ namespace Regnvejrsstatistik
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //user inputs amount of values first
+            //then user inputs the values themselves
+
+            //afterwards, the lowest, highest and avereage of those values must be found
+            bool totalInputted = false;
+
+            Console.WriteLine("Enter the total amount of values below: ");
+            string totalValStr = Console.ReadLine();
+            int totalVal = 0;
+
+            if(int.TryParse(totalValStr, out totalVal))
+            {
+                if(totalVal > 0) 
+                {
+                    totalInputted = true;
+                    Console.WriteLine("Great success!");
+                }
+
+            }
+            else Console.WriteLine();
+
+            double[] values = new double[totalVal];
         }
     }
 }
